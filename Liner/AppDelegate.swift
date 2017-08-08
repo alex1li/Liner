1ad880f70f49b6b399cb822ce090b36b6cc53c53
Alex Li <alexxli789@gmail.com>
2017-08-07 20:25:24 -0400

    testing 2 at the same time

--- Liner/AppDelegate.swift
+++ Liner/AppDelegate.swift
@@ -18,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         window = UIWindow()
         
+        window = UIApplication()
         
         // Override point for customization after application launch.
         return true
