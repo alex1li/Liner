7cec8f71ab85e99a77dffdca88f6a16d93c2bd1a
Rohan Patel <rohanpatel@dhcp-rhodes-535.eduroam.cornell.edu>
2017-09-04 18:06:53 -0400

    checkifissuesoccurred

---
+++ Liner/CustomPersonViewController.swift
@@ -0,0 +1,80 @@
+//
+//  CustomPersonViewController.swift
+//  Liner
+//
+//  Created by Rohan Patel on 9/4/17.
+//  Copyright © 2017 Alexander Li. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+
+class CustomPersonViewController: UIViewController {
+    
+    var Name: UITextField!
+    var addButton: UIButton!
+    var ref: DatabaseReference!
+
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        ref = Database.database().reference()
+        
+        view.backgroundColor = .white
+        
+        
+        
+        Name = UITextField(frame: CGRect(x: 30, y: 230,width: 320, height: 40))
+        Name.borderStyle = UITextBorderStyle.roundedRect
+        //email.backgroundColor = .white
+        view.addSubview(Name)
+
+        addButton = UIButton(frame: CGRect(x: 150, y: 400, width: 100, height: 50))
+        addButton.setTitle("Add Person", for: .normal)
+        addButton.backgroundColor = .blue
+        addButton.setTitleColor(.white, for: .normal)
+        addButton.addTarget(self, action:#selector(add), for: .touchUpInside)
+        view.addSubview(addButton)
+
+        
+        
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    func add() {
+        
+        
+        let user = Auth.auth().currentUser
+        let currentDateTime = Date()
+        
+        ref?.child("Queues").child((user?.displayName)!).child(String(describing: currentDateTime)).setValue(Name.text)
+        
+                
+        self.navigationController?.popViewController(animated: true)
+
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Liner/ManagerViewController.swift
+++ Liner/ManagerViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ManagerViewController.swift
-//
+//  
 //
 //  Created by Rohan Patel on 8/10/17.
 //
@@ -10,7 +10,6 @@ import FirebaseAuth
 import Firebase
 import FirebaseDatabase
 import UIKit
-
 
 class ManagerViewController: UIViewController, UITableViewDataSource, UITableViewDelegate  {
     
@@ -24,13 +23,18 @@ class ManagerViewController: UIViewController, UITableViewDataSource, UITableVie
     var handle2: DatabaseHandle?
     var popButton: UIButton!
     var addButton: UIButton!
-    
-    var user = Auth.auth().currentUser
-    
+    var closeButton: UIButton!
     var myList:[String] = []
     var keyList:[String] = []
+    var changeRequest: UserProfileChangeRequest?
+
     
+
     
+    var user = Auth.auth().currentUser
+
+
+
     override func viewDidLoad() {
         
         super.viewDidLoad()
@@ -38,72 +42,91 @@ class ManagerViewController: UIViewController, UITableViewDataSource, UITableVie
         self.title = user?.displayName
         
         ref = Database.database().reference()
-        
+
         if(user?.displayName == nil){
             
             navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Create Queue", style: .plain, target: self, action: #selector(createQueue))
-            
+
             createAlert(title: "Hey Manager!", message: "Click Create Queue to create your queue")
             
             
         }
         else {
-            
+
             handles()
-            navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add", style: .plain, target: self, action: #selector(addAnonymous))
-            
+            navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add", style: .plain, target: self, action: #selector(addSomeone))
+
             
         }
         
         
         tableView = UITableView(frame: view.frame)
         
-        
-        
         tableView.dataSource = self
         tableView.delegate = self
         
-        
+
         view.addSubview(tableView)
         
-        popButton = UIButton(frame: CGRect(x: 110, y: 500, width: 100, height: 30))
+        
+        closeButton = UIButton(frame: CGRect(x: 160, y: 500, width: 100, height: 30))
+        closeButton.setTitle("Close", for: .normal)
+        closeButton.setTitleColor(.black, for: .normal)
+        closeButton.backgroundColor = .white
+        closeButton.addTarget(self, action: #selector(close), for: .touchUpInside)
+        view.addSubview(closeButton)
+
+        
+        popButton = UIButton(frame: CGRect(x: 110, y: 550, width: 100, height: 30))
         popButton.setTitle("Pop", for: .normal)
         popButton.setTitleColor(.black, for: .normal)
         popButton.backgroundColor = .white
         popButton.addTarget(self, action: #selector(pop), for: .touchUpInside)
         view.addSubview(popButton)
         
-        
+
         // Do any additional setup after loading the view.
+    }
+    
+    func close() {
+        
+        
+        
     }
     
     func pop() {
         
-        
+
         if(myList.count > 1) {
-            print(self.keyList[1])
-            self.myList.remove(at: 1)
-            self.ref?.child("Queues").child((self.user?.displayName!)!).child(self.keyList[1]).setValue(nil)
-            self.keyList.remove(at: 1)
-            
-            tableView.reloadData()
-            
-            view.reloadInputViews()
-        }
+        print(keyList[1])
+        myList.remove(at: 1)
+        ref?.child("Queues").child((self.user?.displayName!)!).child(self.keyList[1]).setValue(nil)
+        self.keyList.remove(at: 1)
+
+        tableView.reloadData()
         
+        view.reloadInputViews()
+        }
+
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return self.myList.count
+        return myList.count
         
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
         let cell = UITableViewCell(style: .default , reuseIdentifier: "")
         cell.backgroundColor = UIColor(white: 1, alpha: 0.1)
         
-        cell.textLabel?.text = self.myList[indexPath.row]
+        if(indexPath.row == myList.count - 1) {
+            return cell
+        }
+        
+        cell.textLabel?.text = myList[indexPath.row + 1]
         
         return cell
     }
@@ -116,12 +139,12 @@ class ManagerViewController: UIViewController, UITableViewDataSource, UITableVie
         alert.addAction(UIAlertAction(title: "Cancel",
                                       style: UIAlertActionStyle.default,
                                       handler: {(alert: UIAlertAction!) in print("cancelled")}))
-        
+
         alert.addAction(UIAlertAction(title: "Create Queue",
                                       style: UIAlertActionStyle.default,
                                       handler: {(alert: UIAlertAction!) in self.createQueue()}))
         
-        
+
         self.present(alert, animated: true, completion: nil)
         
     }
@@ -133,10 +156,10 @@ class ManagerViewController: UIViewController, UITableViewDataSource, UITableVie
         self.navigationController?.pushViewController(ManagerCreateQueueController(), animated: true)
         
     }
+
     
-    
-    
-    
+
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -157,33 +180,41 @@ class ManagerViewController: UIViewController, UITableViewDataSource, UITableVie
                 self.keyList.append(item)
                 
             }
-            
+
         })
         
         handle2 = ref?.child("Queues").child((user?.displayName)!).observe(.childRemoved, with: { (snapshot) in
             //***Adding keys to myList instead of the values now to allow for easy deleting of top person
             if (snapshot.value as! String?) != nil {
                 
+                print("freshstart")
+                
+                print("hallo")
                 if(self.myList.index(of: snapshot.value as! String) != nil) {
+                
                     
-                    let index1 = self.keyList.index(of: snapshot.key as! String)
-                    self.keyList.remove(at: index1!)
-                    self.myList.remove(at: index1!)
-                    
+                print("asper")
+                let index1 = self.keyList.index(of: snapshot.key as! String)
+                print(index1)
+                print("Alexander")
+                self.keyList.remove(at: index1!)
+                print("hello")
+                self.myList.remove(at: index1!)
+                print("Hallo")
                 }
                 
                 self.tableView.reloadData()
             }
-            
+
             
             
         })
         
-        
+
         
     }
     
-    
+
     override func viewDidAppear(_ animated: Bool) {
         if(handle == nil && user?.displayName != nil) {
             
@@ -191,37 +222,38 @@ class ManagerViewController: UIViewController, UITableViewDataSource, UITableVie
             
         }
         if(user?.displayName != nil) {
-            navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add", style: .plain, target: self, action: #selector(addAnonymous))
-            
-        }
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add", style: .plain, target: self, action: #selector(addSomeone))
         
+        }
+    
     }
     
     override func setEditing(_ editing: Bool, animated: Bool) {
         
         if(!tableView.isEditing)
         {
-            tableView.isEditing = true;
-            // self.editButtonItem.title = "Done"
+        tableView.isEditing = true;
+       // self.editButtonItem.title = "Done"
         }
-            
+        
         else {
-            tableView.isEditing = false;
-            // self.editButtonItem.title = "Edit"
-            
+        tableView.isEditing = false;
+        // self.editButtonItem.title = "Edit"
+
         }
         
         
     }
     
-    func addAnonymous() {
+    func addSomeone() {
         
         let user = Auth.auth().currentUser
         let currentDateTime = Date()
-        ref?.child("Queues").child((user?.displayName)!).child(String(describing: currentDateTime)).setValue("Anonymous")
+        
+        self.navigationController?.pushViewController(CustomPersonViewController(), animated: true)
+        
+        
         tableView.reloadData()
-        
-        
         
     }
     
@@ -230,27 +262,32 @@ class ManagerViewController: UIViewController, UITableViewDataSource, UITableVie
         let delete = UITableViewRowAction(style: .destructive, title: "Delete") { (action, indexPath) in
             // delete item at indexPath
             
-            self.ref?.child("Queues").child((self.user?.displayName!)!).child(self.keyList[indexPath.row]).setValue(nil)
+            if(indexPath.row == 0) {
+                return
+            }
+            
+            
+        self.ref?.child("Queues").child((self.user?.displayName!)!).child(self.keyList[indexPath.row]).setValue(nil)
             tableView.beginUpdates()
             self.myList.remove(at: indexPath.row)
+            self.keyList.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .automatic)
             tableView.endUpdates()
-            
+
         }
-        
+    
         
         return [delete]
-        
+
     }
     /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
-}
+    // MARK: - Navigation
 
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
